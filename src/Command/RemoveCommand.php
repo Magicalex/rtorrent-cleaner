830f390a42e93c0cc772d0efb273ffbd4e720560
Magicalex <magicalex@mondedie.fr>
2018-12-05 23:58:02 +0100

    fix(): rm and report command

--- src/Command/RemoveCommand.php
+++ src/Command/RemoveCommand.php
@@ -5,6 +5,7 @@ namespace RtorrentCleaner\Command;
 use RtorrentCleaner\Utils\ListingFile;
 use RtorrentCleaner\Utils\Str;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
