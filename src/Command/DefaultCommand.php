7f703d733451b06da1fe06336f20ac95b7c8b94f
Magicalex <magicalex@mondedie.fr>
2019-02-10 19:21:02 +0100

    feat(): improve Application class

---
+++ src/Command/DefaultCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rtorrent\Cleaner\Command;
+
+use Symfony\Component\Console\Command\ListCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DefaultCommand extends ListCommand
+{
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            "      _                            _          _",
+            " _ __| |_ ___  _ __ _ __ ___ _ __ | |_    ___| | ___  __ _ _ __   ___ _ __",
+            "| '__| __/ _ \| '__| '__/ _ \ '_ \| __|  / __| |/ _ \/ _` | '_ \ / _ \ '__|",
+            "| |  | || (_) | |  | | |  __/ | | | |_  | (__| |  __/ (_| | | | |  __/ |",
+            "|_|   \__\___/|_|  |_|  \___|_| |_|\__|  \___|_|\___|\__,_|_| |_|\___|_|"
+        ]);
+
+        parent::execute($input, $output);
+    }
+}
--- src/Console/Application.php
+++ src/Console/Application.php
@@ -2,42 +2,29 @@
 
 namespace Rtorrent\Cleaner\Console;
 
-use Rtorrent\Cleaner\Command\MoveCommand;
-use Rtorrent\Cleaner\Command\RemoveCommand;
-use Rtorrent\Cleaner\Command\ReportCommand;
-use Rtorrent\Cleaner\Command\TorrentsCommand;
-use Symfony\Component\Console\Application as App;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class Application
+class Application extends BaseApplication
 {
-    private $app;
-    private $version = '0.6.0';
-    private $logo = "      _                            _          _
- _ __| |_ ___  _ __ _ __ ___ _ __ | |_    ___| | ___  __ _ _ __   ___ _ __
-| '__| __/ _ \| '__| '__/ _ \ '_ \| __|  / __| |/ _ \/ _` | '_ \ / _ \ '__|
-| |  | || (_) | |  | | |  __/ | | | |_  | (__| |  __/ (_| | | | |  __/ |
-|_|   \__\___/|_|  |_|  \___|_| |_|\__|  \___|_|\___|\__,_|_| |_|\___|_|";
+    private $version = '0.6.1';
+    private $name = 'rtorrent-cleaner';
 
     public function __construct()
     {
-        $this->app = new App();
-        $this->configure();
-    }
-
-    public function run()
-    {
-        $this->app->run();
-    }
-
-    protected function configure()
-    {
-        $this->app->setName($this->logo);
-        $this->app->setVersion("\n<fg=white>rtorrent-cleaner version</> <fg=yellow>{$this->version}</>");
-        $this->app->addCommands([
-            new MoveCommand(),
-            new ReportCommand(),
-            new RemoveCommand(),
-            new TorrentsCommand()
+        parent::__construct($this->name, '<fg=white>version</> <comment>'.$this->version.'</>');
+        $this->addCommands([
+            new \Rtorrent\Cleaner\Command\MoveCommand(),
+            new \Rtorrent\Cleaner\Command\ReportCommand(),
+            new \Rtorrent\Cleaner\Command\RemoveCommand(),
+            new \Rtorrent\Cleaner\Command\DefaultCommand(),
+            new \Rtorrent\Cleaner\Command\TorrentsCommand()
         ]);
+    }
+
+    public function run(InputInterface $input = null, OutputInterface $output = null)
+    {
+        return parent::run($input, $output);
     }
 }
