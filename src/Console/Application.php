7c31229e286cf8129e043cf867dc43f46f7e5d01
Magicalex <magicalex@mondedie.fr>
2019-05-25 04:11:57 +0200

    feat(): add entrypoint for the new release

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends BaseApplication
 {
-    private $version = '0.7.1';
+    private $version = '0.8.0';
     private $name = 'rtorrent-cleaner';
 
     public function __construct()
